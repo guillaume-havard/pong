d5a9e7e177351aa537298a7a5d7c034144852dd7
Guillaume Havard <guill.havard@gmail.com>
2014-10-25 10:49:56 +0200

    Add a Ball

--- src/elements.py
+++ src/elements.py
@@ -1,5 +1,8 @@
 #! /usr/bin/python3
 
+import random
+import pygame
+
 
 class Player:
     """
@@ -8,14 +11,118 @@ class Player:
         color
         position and size
     """
-    
-    def __init__(self, color, rect):
+
+    def __init__(self, level, color, rect):
         """
-        color [in] : color of the player
+        level pygame.Rect [in] : reference to the level Rect
+        color pygame.Color [in] : color of the player
         rect pygame.Rect [in] : pos and size of the paddle
         """
+        self.level = level
         self.color = color
         self.rect = rect
         self.score = 0
-        
-    
+
+        self.speed = 10
+        self.moving = 0
+
+    def move_up(self):
+        """
+        Set an upward mouvement
+        """
+        self.moving = -1
+
+    def move_down(self):
+        """
+        Set an upward mouvement
+        """
+        self.moving = 1
+
+    def move_stop(self):
+        """
+        Stop movements
+        """
+        self.moving = 0
+
+    def step(self):
+        """
+        Step in the player state
+        """
+        self.rect.bottom = self.rect.bottom + self.moving * self.speed
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+        if self.rect.bottom > self.level.bottom:
+            self.rect.bottom = self.level.bottom
+
+
+class Ball:
+    """
+    pos
+    size
+    speed
+    direction
+    """
+
+    def __init__(self, level, color, pos):
+        """
+        level pygame.Rect [in] : reference to the level Rect
+        color pygame.Color [in] : color of the player
+        pos set of 2 int [in] : position of the ball
+        size [in] :
+        speed [in] :
+        direction rad/deg ? [in] :
+        """
+        self.level = level
+        self.color = color
+        self.pos = [int(x) for x in pos]
+        self.size = 20
+        self.speed_x = 10 + random.randint(0, 10)
+        self.speed_x *= random.choice((-1, 1))
+        self.speed_y = 10 + random.randint(0, 10)
+        self.speed_y *= random.choice((-1, 1))
+
+        print("Nouvelle Balle")
+        print(self.pos)
+        print(self.speed_x, self.speed_y)
+
+    def step(self):
+        """
+        one ball step
+        return a non empty string if the ball fall on one side
+
+        /!\ the ball is out when it touches the floor and ceiling
+        """
+        self.pos[0] += self.speed_x
+        self.pos[1] += self.speed_y
+
+        if not 0 <= self.pos[1] <= self.level.height:
+            self.speed_y = -self.speed_y
+
+        rect_coll = pygame.Rect((0, 0), (self.size, self.size))
+        rect_coll.center = self.pos
+
+        if not self.level.contains(rect_coll):
+            if self.pos[0] < self.level.width / 2:
+                return "left"
+            elif self.pos[0] > self.level.width / 2:
+                return "right"
+            else:
+                print("magic", self.pos)
+                return "magic"
+        else:
+            print("inside")
+
+    def test_collisions(self, players):
+        """
+        test if coolision with players
+        Act accordingly
+        """
+        rect_coll = pygame.Rect((0, 0), (self.size, self.size))
+        rect_coll.center = self.pos
+
+        if rect_coll.collidelist(players) != -1:
+            self.speed_x = -self.speed_x
+
+
--- src/pong.py
+++ src/pong.py
@@ -7,7 +7,7 @@
     level
     ball (multi ?)
     2 players (or more ? 4)
-    
+
     For the moment, basic version
 """
 
@@ -29,15 +29,20 @@ blue_color = pygame.Color(0, 0, 255)
 white_color = pygame.Color(255, 255, 255)
 black_color = pygame.Color(0, 0, 0)
 
-player1 = Player(red_color, pygame.Rect((0, 0), (20, 100)))
-player1.rect.center = (30, WINDOW_HEIGHT/2)
-player2 = Player(blue_color, pygame.Rect((0, 0), (20, 100)))
-player2.rect.center = (WINDOW_WIDTH-30, WINDOW_HEIGHT/2)
+player1 = Player(windos_surface_obj.get_rect(), red_color,
+                 pygame.Rect((0, 0), (20, 100)))
+player1.rect.center = (30, WINDOW_HEIGHT / 2)
+player2 = Player(windos_surface_obj.get_rect(), blue_color,
+                 pygame.Rect((0, 0), (20, 100)))
+player2.rect.center = (WINDOW_WIDTH - 30, WINDOW_HEIGHT / 2)
+
+ball = Ball(windos_surface_obj.get_rect(), white_color,
+            (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 while True:
-    
+
     windos_surface_obj.fill(black_color)
-    
+
     # pygame.event.get() returns a list off all Event objects that happened
     # since the last time get() was called.
     # The Event object has type, pos, key and other attributes depending on
@@ -51,18 +56,48 @@ while True:
             pass
         elif event.type == MOUSEBUTTONUP:
             pass
-                            
+
         elif event.type == KEYDOWN:
-            if event.key in (K_LEFT, K_RIGHT, K_UP, K_DOWN):
-                msg = ("Arrow key pressed")
-            if event.key == K_a:
-                msg = "'a' key pressed"
-            if event.key == K_ESCAPE:
+            if event.key == K_UP:
+                player2.move_up()
+            elif event.key == K_DOWN:
+                player2.move_down()
+            elif event.key == K_a:
+                player1.move_up()
+            elif event.key == K_q:
+                player1.move_down()
+            elif event.key == K_ESCAPE:
                 pygame.event.post(pygame.event.Event(QUIT))
-    
-    pygame.draw.rect(windos_surface_obj, red_color, player1.rect)
-    pygame.draw.rect(windos_surface_obj, red_color, player2.rect)
-    
-    
-    pygame.display.update()   
+        elif event.type == KEYUP:
+            if event.key in (K_UP, K_DOWN):
+                player2.move_stop()
+            if event.key in (K_a, K_q):
+                player1.move_stop()
+
+    player1.step()
+    player2.step()
+    ret_ball = ball.step()
+
+    # ball collision test
+    ball.test_collisions((player1, player2))
+
+    if ret_ball == "left":
+        print("Le joueur 1 a gagne")
+        player1.score += 1
+        ball = Ball(windos_surface_obj.get_rect(), white_color,
+                    (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+    elif ret_ball == "right":
+        print("Le joueur 2 a gagne")
+        player2.score += 1
+        ball = Ball(windos_surface_obj.get_rect(), white_color,
+                    (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+
+
+    pygame.draw.rect(windos_surface_obj, player1.color, player1.rect)
+    pygame.draw.rect(windos_surface_obj, player2.color, player2.rect)
+    #print(ball.pos)
+    pygame.draw.circle(windos_surface_obj, ball.color, ball.pos, ball.size)
+
+    pygame.display.update()
     fps_clock.tick(30)
