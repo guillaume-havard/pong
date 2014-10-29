740e18b49c0408047906d905343a506a02aa6533
Guillaume Havard <guill.havard@gmail.com>
2014-10-29 22:25:27 +0100

    The wall ball is taken into account when bouncing.

--- src/elements.py
+++ src/elements.py
@@ -48,7 +48,7 @@ class Player:
         """
         Step in the player state
         """
-        self.rect.bottom = self.rect.bottom + self.moving * self.speed
+        self.rect.bottom += self.moving * self.speed
 
         if self.rect.top < 0:
             self.rect.top = 0
@@ -70,56 +70,47 @@ class Ball:
         level pygame.Rect [in] : reference to the level Rect
         color pygame.Color [in] : color of the player
         pos set of 2 int [in] : position of the ball
-        size [in] :
+        radius [in] :
         speed [in] :
         direction rad/deg ? [in] :
         """
         self.level = level
         self.color = color
         self.pos = [int(x) for x in pos]
-        self.size = 20
-        self.speed_x = 0 + random.randint(0, 10)
+        self.radius = 10
+        self.speed_x = 5 + random.randint(0, 5)
         self.speed_x *= random.choice((-1, 1))
-        self.speed_y = 0 + random.randint(0, 10)
+        self.speed_y = 0 + random.randint(0, 5)
         self.speed_y *= random.choice((-1, 1))
 
         print("Nouvelle Balle")
         print(self.pos)
         print(self.speed_x, self.speed_y)
 
-    def step(self):
+    def step(self, players):
         """
         one ball step
+        test if collision with players
+        Act accordingly
         return a non empty string if the ball fall on one side
 
-        /!\ Make the ball bounce when its border touch the paddle or border        
+        /!\ Make the ball bounce when its circumference touch the paddle or border
         """
         self.pos[0] += self.speed_x
         self.pos[1] += self.speed_y
 
-        if not 0 <= self.pos[1] <= self.level.height:
-            self.speed_y = -self.speed_y
-
-        rect_coll = pygame.Rect((0, 0), (self.size, self.size))
+        rect_coll = pygame.Rect((0, 0), (self.radius * 2, self.radius * 2))
         rect_coll.center = self.pos
 
+        if rect_coll.collidelist(players) != -1:
+            self.speed_x = -self.speed_x
+
+        if not 0 <= self.pos[1] <= self.level.height:
+            self.speed_y = -self.speed_y
         
         if self.pos[0] < 0:
             return "left"
         elif self.pos[0] > self.level.width:
             return "right"            
         else:
-            return "inside"
-
-    def test_collisions(self, players):
-        """
-        test if coolision with players
-        Act accordingly
-        """
-        rect_coll = pygame.Rect((0, 0), (self.size, self.size))
-        rect_coll.center = self.pos
-
-        if rect_coll.collidelist(players) != -1:
-            self.speed_x = -self.speed_x
-
-
+            return ""
